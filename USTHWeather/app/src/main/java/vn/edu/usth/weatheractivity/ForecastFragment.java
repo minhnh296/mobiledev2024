3e39d171a6211e884b24d7e4a42d3810f2b15495
minh <hacminh204@gmail.com>
2024-08-30 15:47:28 +0700

    Lab3

--- USTHWeather/app/src/main/java/vn/edu/usth/weatheractivity/ForecastFragment.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weatheractivity/ForecastFragment.java
@@ -1,6 +1,7 @@
 package vn.edu.usth.weatheractivity;
 
 import android.graphics.Color;
+import android.media.Image;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,6 +9,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,6 +25,18 @@ public class ForecastFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_forecast, container, false);
         view.setBackgroundColor(0x2000FF00);
+
+        LinearLayout frag = (LinearLayout) view.findViewById(R.id.frag);
+        frag.setOrientation(LinearLayout.HORIZONTAL);
+
+        TextView textView = new TextView(getContext());
+        textView.setText("Thursday");
+
+        ImageView imageView = new ImageView(getContext());
+        imageView.setImageResource(R.drawable.weathernews_648198);
+
+        frag.addView(imageView);
+        frag.addView(textView);
         return view;
     }
 }
