19c6ebf9e17a0c3e69347886eb36251c4774c208
minhnh296 <hacminh2004@gmail.com>
2024-09-04 13:59:57 +0700

    Labwork 1+2+3+4+5

--- USTHWeather/app/src/main/java/vn/edu/usth/weatheractivity/ForecastFragment.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weatheractivity/ForecastFragment.java
@@ -1,42 +1,31 @@
 package vn.edu.usth.weatheractivity;
 
-import android.graphics.Color;
-import android.media.Image;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ForecastFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ForecastFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_forecast, container, false);
-        view.setBackgroundColor(0x2000FF00);
-
-        LinearLayout frag = (LinearLayout) view.findViewById(R.id.frag);
-        frag.setOrientation(LinearLayout.HORIZONTAL);
-
-        TextView textView = new TextView(getContext());
-        textView.setText("Thursday");
-
-        ImageView imageView = new ImageView(getContext());
-        imageView.setImageResource(R.drawable.weathernews_648198);
-
-        frag.addView(imageView);
-        frag.addView(textView);
+//        view.setBackgroundColor(0x2000FF00);
+//        LinearLayout frag = (LinearLayout) view.findViewById(R.id.frag);
+//        frag.setOrientation(LinearLayout.HORIZONTAL);
+//
+//        TextView textView = new TextView(getContext());
+//        textView.setText("Thursday");
+//
+//        ImageView imageView = new ImageView(getContext());
+//        imageView.setImageResource(R.drawable.weathernews_648198);
+//
+//        frag.addView(imageView);
+//        frag.addView(textView);
         return view;
     }
 }
